92d50a4126c01c81418388787a30b1a8bb8ef84a
Bruno Gonçalves <brunogon463@gmail.com>
2023-07-16 19:51:05 -0300

    Initial commit

---
+++ aula11/Sistema de Multas/ex008.js
@@ -0,0 +1,6 @@
+var vel = 90
+console.log(`A sua velocidade é ${vel}Km/H`);
+if ( vel > 80) {
+    console.log('Você ultrapassou o limite de velocidade, MULTADO!');
+}
+console.log('Dirija com Cinto de Segurança');
